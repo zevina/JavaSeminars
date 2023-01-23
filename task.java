2cc09d7d3f3d6ce176e31de606e136c1977562fa
Zevina Lena <zevina.l@yandex.ru>
2023-01-24 04:00:54 +0700

    add task.java

--- task.java
+++ task.java
@@ -1,3 +1,5 @@
+// Решено совместно с Рябовым Андреем
+
 /**
  * На вход некоторому исполнителю подаётся два числа (a, b).
  * У исполнителя есть две команды
@@ -15,16 +17,57 @@
  * ответ: нет решения.
  * 
  */
+// import java.util.ArrayList;
+// import java.util.List;
+import java.util.*;
 
 public class task {
 
-  // 1. Получение данных от пользователя
   public static void main(String[] args) {
     int a = Library.getNumberByUser("Введите число a: ");
     int b = Library.getNumberByUser("Введите число b: ");
     int c = Library.getNumberByUser("Введите число c: ");
     int d = Library.getNumberByUser("Введите число d: ");
 
+    System.out.println(GetOrderCommands(a, b, c, d));
   }
 
+  /**
+   * Метод вычисляет минимальное количество команд для превращения числа y в число
+   * x, если это возможно
+   * 
+   * @param x первое число
+   * @param y второе число
+   * @param z команда к1: увеличить x в z раз
+   * @param p команда к2: увеличить x на число p
+   * @return список команд в порядке их выполнения
+   */
+  public static String GetOrderCommands(int x, int y, int z, int p) {
+
+    List<String> resultList = new ArrayList<>();
+
+    if (y <= x) {
+      System.out.println("нет решения");
+      return null;
+
+    } else {
+
+      while (y > x) {
+        if (y % z == 0 && y / z >= x) {
+          y /= z;
+          resultList.add("k1");
+
+        } else {
+          if ((y - p) >= x) {
+            y -= p;
+            resultList.add("k2");
+
+          }
+        }
+      }
+      Collections.reverse(resultList);
+      String res = String.join(", ", resultList);
+      return res;
+    }
+  }
 }
